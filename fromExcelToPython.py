a5afeede77d9dc8c50b30462c9140a2725f4be4e
likp <sincedeeper@163.com>
2019-11-30 23:03:58 +0800

    test where function in fromExcelToPython.py

--- fromExcelToPython.py
+++ fromExcelToPython.py
@@ -1,14 +1,29 @@
 import pandas as pd
 import numpy as np
-df = pd.DataFrame({"id":[1001,1002,1003,1004,1005,1006],
-                    "date":pd.date_range('20130102', periods=6),
-                    "city":['Beijing ', 'SH', ' guangzhou ','Shenzhen','shanghai', 'BEIJING'],
-                    "age":[23,44,54,32,34,32],
-                    "category":['100-A','100-B','110-A','110-C','210-A','130-F'],
-                    "price":[1200,np.nan,2133,5433,np.nan,4432]},
-                    columns =['id','date','city','category','age','price'])
-df['city']=df['city'].str.lower()
+
+df = pd.DataFrame({"id": [1001, 1002, 1003, 1004, 1005, 1006],
+                   "date": pd.date_range('20130102', periods=6),
+                   "city": ['Beijing ', 'SH', ' guangzhou ', 'Shenzhen', 'shanghai', 'BEIJING'],
+                   "age": [23, 44, 54, 32, 34, 32],
+                   "category": ['100-A', '100-B', '110-A', '110-C', '210-A', '130-F'],
+                   "price": [1200, np.nan, 2133, 5433, np.nan, 4432]},
+                  columns=['id', 'date', 'city', 'category', 'age', 'price'])
+df['city'] = df['city'].str.lower()
 print(df)
 
-#输出某列的唯一值
-print ("输出city列的城市名称，去除重复项 {0}".format(df['city'].unique()))
+df1 = pd.DataFrame({"id": [1001, 1002, 1003, 1004, 1005, 1006, 1007, 1008],
+                    "gender": ['male', 'female', 'male', 'female', 'male', 'female', 'male', 'female'],
+                    "pay": ['Y', 'N', 'Y', 'Y', 'N', 'Y', 'N', 'Y', ],
+                    "m-point": [10, 12, 20, 40, 40, 40, 30, 20]})
+
+print(df1)
+# 数据表匹配合并
+df_inner = pd.merge(df1, df, how='inner')
+print(df_inner)
+# 如果price列的值>3000，group列显示high，否则显示low
+df_inner['group'] = np.where(df_inner['price'] > 3000, 'high', 'low')
+# 对复合多个条件的数据进行分组标记
+df_inner.loc[(df_inner['city'] == 'beijing') & (df_inner['price'] >= 4000), 'sign'] = 1
+print(df_inner)
+# #输出某列的唯一值
+# print ("输出city列的城市名称，去除重复项 {0}".format(df['city'].unique()))
